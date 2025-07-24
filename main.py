07fe8ced61a863d20f523fed67217425ba0d9bf7
Vicente Echegaray <viechegaray@alumnos.uai.cl>
2025-07-24 16:57:22 -0400

    kk

--- main.py
+++ main.py
@@ -17,22 +17,19 @@ async def voice():
     twiml = """
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Gather input="speech" timeout="5" speechTimeout="auto" language="es-US" action="/handle_recording">
-    <Say language="es-US" voice="es-US-Chirp3-HD-Charon">
-      Hola, bienvenido. Por favor, dime lo que desees grabar.
-    </Say>
-  </Gather>
+  <Say language="es-US" voice="es-US-Chirp3-HD-Charon">
+    Hola, bienvenido. Por favor, dime lo que desees grabar.
+  </Say>
 
   <Record
     maxLength="120"
     transcribe="true"
     recordingTrack="both_tracks"
     transcribeLanguage="es-CL"
-    action="/process_recording"
   />
 
   <Say language="es-US" voice="es-US-Chirp3-HD-Charon">
-    No he recibido ninguna entrada. Adiós.
+    Gracias por tu grabación. Adiós.
   </Say>
 </Response>
 """
